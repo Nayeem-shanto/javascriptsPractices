4e5f77f04bd698081f89562d3a6216da65ad5d33
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2025-07-29 15:41:04 +0600

    repository created

---
+++ module13.js
@@ -0,0 +1,24 @@
+
+console.log("Let's Check");
+/* 
+    --------------------JavaScripts Introduction:-----------------
+    1. variable and how to declare it.
+    2. different types of variable.
+    3. data type. 
+    4. naming convention.
+    5. arithmetic operation.
+    6. 
+
+*/
+
+var tk=1000;
+var applePrice= 400;
+var orangePrice= 500;
+
+var sum =(applePrice + orangePrice);
+console.log(sum);
+var returnAmount = (tk - sum);
+console.log(returnAmount);
+
+
+
---
+++ practice13.js
@@ -0,0 +1,37 @@
+/* Practice task-1 */
+
+/* var tk = 1000;
+console.log("Total amount for buying fruits", tk);
+var applePrice = 370;
+var orangePrice = 330;
+
+var totalPrice =(applePrice + orangePrice);
+console.log("Total Price of apple and orange are =", totalPrice);
+console.log("amount give to the shopkeeper" ,tk);
+var returnAmount= tk-totalPrice;
+console.log("Shop keeper will return", returnAmount, "tk"); */
+
+
+/* Practice Task 2 */
+
+/* var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bengali = 99.50;
+
+var totalMarks = (mathematics + biology + chemistry+physics+bengali);
+console.log("Total marks of the student is =", totalMarks.toFixed(2));
+var totalSubject = '5';
+var averageMarks = (totalMarks/parseInt(totalSubject));
+console.log("The average marks of a student is =", averageMarks.toFixed(2)); */
+
+
+/* Practice task-3 */
+ var number = 119;
+ var dividedNumber = 5;
+//   var remainder = (number%dividedNumber);
+//   console.log("The remained = ", remainder);
+// console.log("The remainder is =", (number%dividedNumber));
+
+
